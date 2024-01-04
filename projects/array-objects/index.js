d2011aaf2885d55516a94fd10b5c96cdbc6a06f9
Margarita Zolotareva <glam-dreamer@yandex.ru>
2024-01-04 15:48:20 +0500

    week-2 done

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,8 +9,11 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el)); // выведет каждый элемент массива
  */
-function forEach() {}
-
+function forEach(array = [1, 2, 3], fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 /*
  Задание 2:
 
@@ -21,8 +24,13 @@ function forEach() {}
    const newArray = map([1, 2, 3], (el) => el ** 2);
    console.log(newArray); // выведет [1, 4, 9]
  */
-function map() {}
-
+function map(array = [4, 5, 6], fn) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(fn(array[i], i, array));
+  }
+  return newArray;
+}
 /*
  Задание 3:
 
@@ -33,8 +41,21 @@ function map() {}
    const sum = reduce([1, 2, 3], (all, current) => all + current);
    console.log(sum); // выведет 6
  */
-function reduce() {}
+function reduce(array = [3, 5, 8], fn, initialValue) {
+  let accumulator = initialValue;
+  let startValue = 0;
 
+  if (initialValue === undefined) {
+    startValue = 1;
+    accumulator = array[0];
+  }
+
+  for (let i = startValue; i < array.length; i++) {
+    accumulator = fn(accumulator, array[i], i, array);
+  }
+
+  return accumulator;
+}
 /*
  Задание 4:
 
@@ -44,6 +65,8 @@ function reduce() {}
    const keys = upperProps({ name: 'Сергей', lastName: 'Петров' });
    console.log(keys) // выведет ['NAME', 'LASTNAME']
  */
-function upperProps() {}
+function upperProps(crows = { name: 'Kaz', lastName: 'Brekker' }) {
+  return Object.keys(crows).map((key) => key.toUpperCase());
+}
 
 export { forEach, map, reduce, upperProps };
--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -20,7 +20,23 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true (потому что все элементы массива меньше 10)
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false (потому что как минимум первый элемент больше 10)
  */
-function isAllTrue(array, fn) {}
+function isAllTrue(array = [3, 5, 6], fn) {
+  if (!Array.isArray(array) || array.length === 0) {
+    throw new Error('empty array');
+  }
+
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (!fn(array[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 /*
  Задание 2:
@@ -42,7 +58,21 @@ function isAllTrue(array, fn) {}
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true (потому что в массиве есть хотя бы один элемент больше 20)
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false (потому что в массиве нет ни одного элемента больше 20)
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array = [1, 2, 3, 4], fn) {
+  if (!Array.isArray(array) || array.length === 0) {
+    throw new Error('empty array');
+  } else if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if(fn(array[i])) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 /*
  Задание 3:
@@ -56,7 +86,22 @@ function isSomeTrue(array, fn) {}
    - fn не является функцией (с текстом "fn is not a function")
      для проверки на функцию вам может помочь оператор typeof
  */
-function returnBadArguments() {}
+function returnBadArguments(fn) {
+  const newArray = [];
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  for (let i = 1; i < arguments.length; i++) {
+    try {
+      fn(arguments[i]);
+    } catch {
+      newArray.push(arguments[i]);
+    }
+  }
+
+  return newArray;
+}
 
 /*
  Задание 4:
@@ -84,7 +129,35 @@ function returnBadArguments() {}
    console.log(calc.div(2, 2)); // выведет 2.5 (10 / 2 / 2)
    console.log(calc.div(2, 0)); // выбросит исключение, потому что один из аргументов равен 0
  */
-function calculator(number) {}
+function calculator(number = 0) {
+  if (typeof number !== 'number') {
+    throw new Error('number is not a number');
+  }
+
+  return {
+    sum(...args) {
+      return args.reduce((total, amount) => total + amount, number);
+    },
+
+    dif(...args) {
+      return args.reduce((total, amount) => total - amount, number);
+    },
+
+    div(...args) {
+      return args.reduce((total, number) => {
+        if (number === 0) {
+          throw new Error('division by 0');
+        } else {
+          return total / number;
+        }
+      }, number);
+    },
+
+    mul(...args) {
+      return args.reduce((total, amount) => total * amount, number);
+    }
+  }
+}
 
 /* При решении задач, постарайтесь использовать отладчик */
 
